56850172d1356b145ee502f54c4111bfc5baca83
Tulius Lima <tuliuslima@Tuliuss-MacBook-Air.local>
2013-12-17 16:03:43 -0200

    primeira classe <3 <3

--- primeiro.rb
+++ primeiro.rb
@@ -1,1 +1,22 @@
-puts "tulius"
+_*_ enconding : utf-8 _*_
+
+class Modo
+
+	def initialize(p1)
+		@p1 = p1
+	end
+
+	def p1
+		@p1
+	end
+
+	def Nome()
+		@nome
+	end
+
+	def Nome=(nome)
+		@nome = nome
+	end
+end
+
+puts "Mister " + Modo.new("Tulius").p1
